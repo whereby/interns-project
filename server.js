0c6c6e39225e18c2a84f81c31f657543c552749c
Håvard Holvik <havard@whereby.com>
2024-04-29 12:42:32 +0200

    Add logging

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const path = require("path");
+const winston = require("winston");
+
 const server = express();
+const logDir = process.env.WHEREBY_INTERNS_LOG_DIR;
 const port = parseInt(process.env.WHEREBY_INTERNS_PORT || "3000");
 
+const logger = new winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      colorize: "all",
+    }),
+    logDir
+      ? new winston.transports.File({
+          filename: path.join(logDir, "webserver.log"),
+        })
+      : null,
+  ].filter(Boolean),
+});
+
 server.get("/", (req, res) => {
+  logger.info("GET /");
+
   res.send("Hello World!");
 });
 
 server.listen(port, () => {
-  console.log(`Example server listening on port ${port}`);
+  logger.info(`Example server listening on port ${port}`);
 });
